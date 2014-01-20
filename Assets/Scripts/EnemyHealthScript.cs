2be326fd13aaf0fa9b8484cbac2a45e35365db5c
Fab <plubie@gmail.com>
2014-01-20 12:08:46 -0500

    completed enemy deaths and health in Fab Test

--- Assets/Scripts/EnemyHealthScript.cs
+++ Assets/Scripts/EnemyHealthScript.cs
@@ -5,6 +5,10 @@ public class EnemyHealthScript : MonoBehaviour {
 
 	public int maxHp;
 	public int currentHp;
+
+	public GameObject explosion1;
+	GameObject spawnedExplosionParticle;
+
 	// Use this for initialization
 	void Start () {
 		currentHp = maxHp;
@@ -13,7 +17,44 @@ public class EnemyHealthScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(currentHp <= 0){
-			GameObject.Destroy(this.gameObject);
+			StartCoroutine(deathAnimation());
 		}
+	}
+
+	public void decreaseHealth(){
+		currentHp -= PlayerVariablesScript.bulletDmg;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Bullet")){
+			decreaseHealth();
+		}
+		if(other.gameObject.CompareTag("Road")){
+			StartCoroutine(spawnAndDestroy());
+		}
+	}
+
+	IEnumerator deathAnimation(){
+		Destroy(gameObject.GetComponent<EnemyTestScript>());
+		spawnedExplosionParticle = (GameObject)GameObject.Instantiate
+			(explosion1, 
+			 gameObject.transform.position, 
+			 Quaternion.identity);
+		spawnedExplosionParticle.transform.parent = gameObject.transform;
+		yield return new WaitForSeconds(0.25f);
+		GameObject.Destroy(spawnedExplosionParticle);
+		/*gameObject.rigidbody.useGravity = true;
+		gameObject.collider.isTrigger = false;*/
+		transform.position = Vector3.Lerp(transform.position, transform.position - new Vector3(0f, 3f, 0f), 0.002f);
+	}
+
+	IEnumerator spawnAndDestroy(){
+		spawnedExplosionParticle = (GameObject)GameObject.Instantiate
+			(explosion1, 
+			 gameObject.transform.position, 
+			 Quaternion.identity);
+		spawnedExplosionParticle.transform.parent = gameObject.transform;
+		yield return new WaitForSeconds(0.25f);
+		GameObject.Destroy(this.gameObject);
 	}
 }

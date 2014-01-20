36962270e4b628832b4066b5e4d0c7616b9142b4
Fab <plubie@gmail.com>
2014-01-20 12:18:05 -0500

    committing changes of Fab Test Scene
    its GONNA BLOW

---
+++ Assets/Scripts/PlayerVariablesScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerVariablesScript : MonoBehaviour{
+	//WEAPON VARIABLES
+	public static string currentWeapon;
+	public static int bulletDmg = 1;
+	public static int fireRate;
+	public static int spreadCount;
+
+
+	//Difficulty Variables
+	
+}

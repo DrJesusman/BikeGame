993d0424eb34d1085fee142051978f02a834f743
Fab <plubie@gmail.com>
2014-01-20 11:09:47 -0500

    adding enemy health script

---
+++ Assets/Scripts/EnemyHealthScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealthScript : MonoBehaviour {
+
+	public int maxHp;
+	public int currentHp;
+	// Use this for initialization
+	void Start () {
+		currentHp = maxHp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(currentHp <= 0){
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}
